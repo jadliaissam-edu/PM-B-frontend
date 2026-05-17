a0ae05bae31163ee160aab835673f810273d3459
quaisse123 <marouanequaisse@gmail.com>
2026-05-17 01:50:24 +0100

    fix: comment out direct IA_BASE_URL to avoid proxy issues and ensure local development consistency

--- src/config/baseURL.tsx
+++ src/config/baseURL.tsx
@@ -1,9 +1,9 @@
 const API_BASE_URL = "/api";
 const AUTH_BASE_URL = `${API_BASE_URL}/auth`;
 // IP directe de l'instance IA pour éviter les problèmes de proxy en développement et en production
-const IA_BASE_URL = "http://88.99.84.245:8000/api/ia";
+// const IA_BASE_URL = "http://88.99.84.245:8000/api/ia";
 // url ai pour le développement local
-// const IA_BASE_URL = "http://localhost:8000/api/ia";
+const IA_BASE_URL = "http://localhost:8000/api/ia";
 const IA_REPO_BASE_URL = IA_BASE_URL;
 
 export { API_BASE_URL, AUTH_BASE_URL, IA_BASE_URL, IA_REPO_BASE_URL };
